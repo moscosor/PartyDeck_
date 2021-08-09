347a087c21373d89f8c84b59bb490355d4270131
KeeganWhelan <whelank@wit.edu>
2021-08-08 23:10:58 -0400

    logic

--- app/src/main/java/com/example/PartyDeck/CoinFlip.java
+++ app/src/main/java/com/example/PartyDeck/CoinFlip.java
@@ -3,12 +3,65 @@ package com.example.PartyDeck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class CoinFlip extends AppCompatActivity {
+
+    public static final Random rand = new Random();
+    private Button flip;
+    private ImageView coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip);
+        flip = (Button) findViewById(R.id.button4);
+        coin = (ImageView) findViewById(R.id.imageView);
+
+        flip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipCoin();
+            }
+        });
+    }
+
+    private void flipCoin() {
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(1000);
+        fadeOut.setFillAfter(true);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                coin.setImageResource(rand.nextFloat() > 0.5 ? R.drawable.quartertails : R.drawable.quarterheads);
+
+                Animation fadeIn = new AlphaAnimation(0, 1);
+                fadeIn.setInterpolator(new DecelerateInterpolator());
+                fadeIn.setDuration(3000);
+                fadeIn.setFillAfter(true);
+
+                coin.startAnimation(fadeIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        coin.startAnimation(fadeOut);
     }
 }
