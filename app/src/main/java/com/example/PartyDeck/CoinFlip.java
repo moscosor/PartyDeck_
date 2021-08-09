41e1e58eb9919ca8bd8fac5a9bb3161c7b5eba4d
KeeganWhelan <whelank@wit.edu>
2021-08-08 20:05:49 -0400

    turn picker and coin flip

---
+++ app/src/main/java/com/example/PartyDeck/CoinFlip.java
@@ -0,0 +1,14 @@
+package com.example.PartyDeck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoinFlip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip);
+    }
+}
