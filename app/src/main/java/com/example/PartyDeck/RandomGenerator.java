13a0a16bce7c140b36ec0b00b7f0f968d39e9a26
moscosor <moscosor@wit.edu>
2021-08-08 23:18:03 -0400

    random generator WIP

---
+++ app/src/main/java/com/example/PartyDeck/RandomGenerator.java
@@ -0,0 +1,14 @@
+package com.example.PartyDeck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomGenerator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_generator);
+    }
+}
